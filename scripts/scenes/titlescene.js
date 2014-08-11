06bc1b1ed62297de88461c013a8545f64b6177f4
daishihmr <daishi.hmr@gmail.com>
2014-08-11 11:17:53 +0900

    title

--- scripts/scenes/titlescene.js
+++ scripts/scenes/titlescene.js
@@ -7,12 +7,34 @@ tm.define("cannon.TitleScene", {
 
         this.fromJSON({
             children: {
+                pre: {
+                    type: "tm.display.Label",
+                    init: ["jsstg 2014", 36],
+                    x: cannon.SC_W * 0.25,
+                    y: cannon.SC_H * (1 - 1 / Math.PHI) - 80,
+                    shadowColor: "white",
+                    shadowBlur: 10,
+                },
                 title: {
                     type: "tm.display.Label",
-                    init: ["Deep Striker", 72],
+                    init: ["Deep Striker", 96],
                     x: cannon.SC_W * 0.5,
                     y: cannon.SC_H * (1 - 1 / Math.PHI),
-                }
+                    fillStyle: tm.graphics.LinearGradient(-cannon.SC_W * 0.5, -100, cannon.SC_W * 0.5, 100)
+                        .addColorStopList([
+                            { offset: 0.3, color: "hsl(220, 50%, 50%)"},
+                            { offset: 0.5, color: "hsl(220, 80%,100%)"},
+                            { offset: 0.7, color: "hsl(220, 50%, 50%)"},
+                        ]).toStyle(),
+                    shadowColor: "white",
+                    shadowBlur: 10,
+                },
+                credit: {
+                    type: "tm.display.Label",
+                    init: ["Copyright (c) 2014 daishi_hmr", 24],
+                    x: cannon.SC_W * 0.5,
+                    y: cannon.SC_H * 0.9,
+                },
             }
         });
 
